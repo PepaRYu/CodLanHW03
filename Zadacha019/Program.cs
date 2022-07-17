363eb4f3c65f2b976731f3f2d77910ce53461b01
DrPRun <sheighd@gmail.com>
2022-07-17 15:13:09 +0300

    reshena pervaya zadacha

--- Zadacha019/Program.cs
+++ Zadacha019/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿int number = Convert.ToInt32(Console.ReadLine());
+int firstDigit = number/10000;
+int secondDigit = (number - firstDigit*10000)/1000;
+int fiveDigit = number%10;
+int fourthDigit = (number%100 - firstDigit)/10;
+
+
+Console.WriteLine("firstDigit: " + firstDigit);
+Console.WriteLine("secondDigit: " + secondDigit);
+Console.WriteLine("fourthDigit: " + fourthDigit);
+Console.WriteLine("fiveDigit: " + fiveDigit);
+if (firstDigit == fiveDigit && secondDigit == fourthDigit){
+    Console.WriteLine($"Число {number} является полиэдром");
+}
